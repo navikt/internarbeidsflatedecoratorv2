d9f19c00565432749fe959a11a802489687423a7
Enger <E148211@ADEO.NO>
2017-02-14 09:21:06 +0100

    PK-37609 PKULV-2246 Fjernet overflødig valideringslogikk + css
    Fjernet outline v/fokus på søkeboksen slik at den vises som rød for ugyldig personnr
    Fjernet funksjonen som sjekker om personnummeret er på vei til å bli riktig siden vi kun lytter på enter

--- internarbeidsflatedecorator-web/src/main/webapp/personsok.js
+++ internarbeidsflatedecorator-web/src/main/webapp/personsok.js
@@ -2,15 +2,12 @@
     var sokefelt = document.getElementById("js-deokorator-sokefelt");
     var valideringslinje = document.getElementById("js-dekorator-feilmelding");
     const ERROR_CLASS = 'error';
+    const ERROR_BANNER_CLASS = 'feilmeldingbanner';
 
     function triggerPersonsokEvent(personnummer) {
         var personsokEvent = new Event('dekorator-hode-personsok');
         personsokEvent.personnummer = personnummer;
         document.dispatchEvent(personsokEvent);
-    }
-
-    function inputErGyldig (input) {
-        return input.match(/^\d*$/) && input.length <= 11;
     }
 
     function inputErGyldigPersonnummer (input) {
@@ -19,20 +16,14 @@
 
     function validerInput (event) {
         const inputfelt = event.target;
-        if (inputErGyldig(inputfelt.value)) {
+        if (inputErGyldigPersonnummer(inputfelt.value)) {
             inputfelt.classList.remove(ERROR_CLASS);
             if (valideringslinje) {
-                valideringslinje.classList.remove('feilmeldingbanner');
-            }
-            console.log(inputfelt.classList);
-            console.log('Riktig');
-            if (inputErGyldigPersonnummer(inputfelt.value)) {
-                console.log('Gyldig');
+                valideringslinje.classList.remove(ERROR_BANNER_CLASS);
             }
         } else {
             inputfelt.classList.add(ERROR_CLASS);
-            valideringslinje.classList.add('feilmeldingbanner');
-            console.log('Feil');
+            valideringslinje.classList.add(ERROR_BANNER_CLASS);
         }
     }
 
