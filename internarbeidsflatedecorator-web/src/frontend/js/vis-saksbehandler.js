ea4e04b06d62f2981d61aa8753b3ed8291428842
Martine Enger <martine.enger@nav.no>
2017-03-01 15:32:22 +0100

    Viser nå navn og ident på saksbehandler hentet fra server. autentisering i fetch-kall er hardkodet inn

--- internarbeidsflatedecorator-web/src/frontend/js/vis-saksbehandler.js
+++ internarbeidsflatedecorator-web/src/frontend/js/vis-saksbehandler.js
@@ -3,16 +3,19 @@ import { fetchToJson } from './rest-utils';
 const visSaksbehandler = (data) => {
     const saksbehandlerNavn = document.getElementById('js-dekorator-saksbehandler-navn');
     const saksbehandlerIdent = document.getElementById('js-dekorator-saksbehandler-ident');
-    console.log(data);
-    saksbehandlerNavn.innerHTML = "Jan Nilelsen Ullmann";
-    saksbehandlerIdent.innerHTML = "(Z606404)";
+    if(!data.navn || !data.ident){
+        return;
+    }
+    saksbehandlerNavn.innerHTML = data.navn;
+    saksbehandlerIdent.innerHTML = `(${data.ident})`;
 };
 
 const hentSaksbehandlerNavnOgIdent = () => {
     const url = `http://${window.location.hostname}:9591/veilarbveileder/tjenester/veileder/me`;
     const credentials = {
+        credentials: 'same-origin',
         headers: {
-            authorization: 'Bearer eyAidHlwIjogIkpXVCIsICJhbGciOiAiUlMyNTYiIH0.eyAiYXVkIjogIk9JREMiLCAic3ViIjogIlo5OTA2MTAiLCAiYXpwIjogIk9JREMiLCAiYXV0aF90aW1lIjogMTQ4ODM2OTc5MSwgImlzcyI6ICJodHRwczovL2lzc28tdC5hZGVvLm5vL29wZW5hbS9vaWRjIiwgImV4cCI6IDE0ODgzNzMzOTEsICJpYXQiOiAxNDg4MzY5NzkxLCAibm9uY2UiOiAiMC41NTcwNjYzMDExMjk2MTY2IiwgImp0aSI6ICJmMTQ5OTYxMC1iYTM4LTQ4MzYtOTExOC0xYzlhNmZmZDRkZjYiIH0.Kh1_B7MFbGiDaPmD3nglU3DsOXscGghNqGzfNaqFmDJEXrhW9JfY2CDgQxem74opl5FYbL2qi51LZ_4nrZsSLRsHAFS7TQgJgOAu0FeZWduzjWXYGhzQGJ84OenfqTWw5YX9WdJqbxZu006lzrvz6Yo1F4VHKk9tbnU2c-H7sJFj0Bhza0YN3TtqsSQlBNkgeZVoH1qiYZK9_QAFh67c4O_EPewHAIq1AHx4ENhwqbhV___aWQ79jm_dgPKgR5CVvoC5h-mIGDyMla1OXLSPbpzw8MDv8g9rcTFDnomfODXfetwF7ppQ42RHvBJTc4QT_dlAVMUzueMO_na70ocLLg'
+            authorization: 'Bearer eyAidHlwIjogIkpXVCIsICJhbGciOiAiUlMyNTYiIH0.eyAiYXVkIjogIk9JREMiLCAic3ViIjogIlo5OTA2MTAiLCAiYXpwIjogIk9JREMiLCAiYXV0aF90aW1lIjogMTQ4ODM3NTA5OSwgImlzcyI6ICJodHRwczovL2lzc28tdC5hZGVvLm5vL29wZW5hbS9vaWRjIiwgImV4cCI6IDE0ODgzNzg2OTksICJpYXQiOiAxNDg4Mzc1MDk5LCAibm9uY2UiOiAiMC42MjcxNDgzMTg0NzE5NDYyIiwgImp0aSI6ICJiMzkyMDQ3YS05N2JjLTQxNDgtOWMyZi0wNTRiOTNmM2Q5NTciIH0.yxB_9KPTacpE5Pk7G1aMKP8iHKx_pfdNKuiiTGaFaNdEdYnT-n5g8etNTJ1CMKMU3U1-qVgFezv5mIZiUzBHql_MJO9Wqj6XztFYS1H2tgDrYEA6mjSBrhrvbxcb2IE12tCnZHDfPygVqUWhwr6H5_OvRAzYBu0Q4j7U9WxHbzyh8BmwgjHSjowP7LiyAg2CHC4KaE1uY-SDnwg0zUkTyfDMtYaq4FrFsCgw3GBzIaBQrTVKLw7wbNzvnY7IxMDPkMs9qmA2x23gHS4qb_glMJdKFJ4TCvpMUg7rbfwiMUq3XeWhckETAh3lLnFUsBnKOxj4mBO0DrnxJv04aRfG3g'
         }
     };
     return fetchToJson(url, credentials)
