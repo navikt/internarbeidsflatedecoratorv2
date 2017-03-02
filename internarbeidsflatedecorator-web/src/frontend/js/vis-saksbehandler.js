2c2fac6756e860c4668ee37a2ed7237dfbe6f0ff
Martine Enger <martine.enger@nav.no>
2017-03-02 14:20:34 +0100

    Småfiks (eslint, feilhåndtering)

--- internarbeidsflatedecorator-web/src/frontend/js/vis-saksbehandler.js
+++ internarbeidsflatedecorator-web/src/frontend/js/vis-saksbehandler.js
@@ -3,7 +3,7 @@ import { fetchToJson } from './rest-utils';
 const VEILEDER_URL = `/veilarbveileder/tjenester/veileder`;
 
 const visSaksbehandler = (data) => {
-    if(!data.navn || !data.ident) {
+    if (!data.navn || !data.ident) {
         return;
     }
     document.getElementById('js-dekorator-saksbehandler-navn').innerHTML = data.navn;
@@ -11,19 +11,29 @@ const visSaksbehandler = (data) => {
 };
 
 const visEnhet = (data) => {
-    if(!data.enhetliste || !data.enhetliste[0].navn) {
+    if (!data.enhetliste || data.enhetliste.length === 0 || !data.enhetliste[0].navn) {
         return;
     }
     document.getElementById('js-dekorator-enhet-navn').innerHTML = data.enhetliste[0].navn;
 };
 
+const handterFeil = (error) => {
+    console.error(error, error.stack); //eslint-disable-line no-console
+};
+
 export const hentSaksbehandler = () => {
     return fetchToJson(`${VEILEDER_URL}/me`)
-        .then(visSaksbehandler);
+        .then(
+            visSaksbehandler,
+            handterFeil
+        );
 };
 
 export const hentEnheter = () => {
     return fetchToJson(`${VEILEDER_URL}/enheter`)
-        .then(visEnhet);
+        .then(
+            visEnhet,
+            handterFeil
+        );
 };
 
