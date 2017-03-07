590101c1c9f624a54000f97681a9f553d2911103
Martine Enger <martine.enger@nav.no>
2017-03-07 08:40:20 +0100

    PK-37609 PKULV-2218: Renamet filer som har 'saksbehandler' i navnet, refakturerte veileder-tester så de blir litt ryddigere

--- internarbeidsflatedecorator-web/src/frontend/js/index.js
+++ internarbeidsflatedecorator-web/src/frontend/js/index.js
@@ -1,7 +1,7 @@
 import init from './init';
 import personsok from './personsok';
 import varsel from './varsel';
-import { hentEnheter, hentVeileder } from './vis-saksbehandler';
+import { hentEnheter, hentVeileder } from './vis-veileder';
 
 document.addEventListener('DOMContentLoaded', init);
 document.addEventListener('DOMContentLoaded', personsok);
