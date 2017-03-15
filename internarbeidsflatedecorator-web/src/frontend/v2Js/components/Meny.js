5082baf72c72d116d3eeb1c23ae94136cd7164fb
Christian Finstad <christian.finstad@nav.no>
2017-03-15 07:53:24 +0100

    PKULV-2377 Konfigurerbar meny

--- internarbeidsflatedecorator-web/src/frontend/v2Js/components/Meny.js
+++ internarbeidsflatedecorator-web/src/frontend/v2Js/components/Meny.js
@@ -1,27 +1,30 @@
 import React, { PropTypes } from 'react';
 
-const Meny = ({ fnr }) => {
+const defaultLenker = ( fnr ) => {
+    return ([
+        [`/sykefravaer/${fnr}`, 'Sykefravær'],
+        ['/moteoversikt', 'Mine dialogmøter'],
+        ['/modiabrukerdialog', 'Modia'],
+        ['/mia', 'Muligheter i Arbeidslivet'],
+    ]);
+};
+
+const getLenker = (lenker) => {
     return (
         <div id="js-dekorator-nav-container" className="dekorator__nav dekorator__nav--apen" aria-controlledby="js-dekorator-toggle-meny">
             <nav id="js-dekorator-nav" className="dekorator__container dekorator__meny">
                 <h2>Lenker</h2>
                 <ul>
-                    <li>
-                        <a href={`/sykefravaer/${fnr}`}>Sykefravær</a>
-                    </li>
-                    <li>
-                        <a href="/moteoversikt">Mine dialogmøter</a>
-                    </li>
-                    <li>
-                        <a href="/modiabrukerdialog">Modia</a>
-                    </li>
-                    <li>
-                        <a href="/mia">Muligheter i Arbeidslivet</a>
-                    </li>
+                    { lenker.map( ([href, tekst]) => <li><a href={href}>{tekst}</a></li> )}
                 </ul>
             </nav>
         </div>
     );
+};
+
+
+const Meny = ({ fnr, egendefinerteLenker }) => {
+    return egendefinerteLenker ? getLenker(egendefinerteLenker) : getLenker(defaultLenker(fnr))
 };
 
 Meny.propTypes = {
