a0837b7861c2524b33fc30d40b83ed51ad738f87
Joakim Lindquister <joakim.lindquister@nav.no>
2017-03-16 17:13:40 +0100

    Fjern link

--- internarbeidsflatedecorator-web/src/frontend/v2Js/components/Overskrift.js
+++ internarbeidsflatedecorator-web/src/frontend/v2Js/components/Overskrift.js
@@ -4,10 +4,10 @@ import NAVLogo from './NAVLogo';
 const Overskrift = ({ applicationName='' }) => {
     return (
         <h1 className="dekorator__tittel">
-            <a href="/" className="dekorator__hode__logo" id="js-dekorator-hjem">
+            <span className="dekorator__hode__logo">
             <NAVLogo />
             <span className="dekorator__hode__tittel">{applicationName}</span>
-            </a>
+            </span>
         </h1>
     );
 };
