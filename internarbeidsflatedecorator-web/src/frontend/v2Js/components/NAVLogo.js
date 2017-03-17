bc48362e29f2ae029f62463c1bb1a4bf3175c6b1
Andreas Lund <andreas.lund@nav.no>
2017-03-17 10:49:19 +0100

    Legger inn noe bedre/mer intuitiv fargegivning til NavLogo'en.

--- internarbeidsflatedecorator-web/src/frontend/v2Js/components/NAVLogo.js
+++ internarbeidsflatedecorator-web/src/frontend/v2Js/components/NAVLogo.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const NAVLogo = () => (
-            <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                viewBox="0 0 148.2 94.9" style="enable-background:new 0 0 148.2 94.9;">
+            <svg viewBox="0 0 148.2 94.9">
                 <title>NAV-logo</title>
                 <g>
-                    <path className="dekorator__navlogo_fill1" d="M115.2,47.8c0,25.4-20.6,46.1-46.1,46.1C43.7,93.9,23,73.3,23,47.8C23,22.4,43.7,1.7,69.1,1.7
+                    <path className="dekorator__svg__hvit" d="M115.2,47.8c0,25.4-20.6,46.1-46.1,46.1C43.7,93.9,23,73.3,23,47.8C23,22.4,43.7,1.7,69.1,1.7
         C94.6,1.7,115.2,22.4,115.2,47.8" />
-                    <polygon className="dekorator__navlogo_fill1" points="0.7,67.9 10.2,44.6 19.2,44.6 9.8,67.9  " />
-                    <polygon className="dekorator__navlogo_fill1" points="117,67.9 126.3,44.6 131.2,44.6 121.9,67.9  " />
-                    <polygon className="dekorator__navlogo_fill1" points="135.3,67.9 144.6,44.6 147.2,44.6 137.9,67.9    " />
-                    <path className="dekorator__navlogo_fill2" d="M108.4,44.6h-8.2c0,0-0.6,0-0.8,0.5L94.9,59l-4.5-13.9c-0.2-0.5-0.8-0.5-0.8-0.5H73.9c-0.3,0-0.6,0.3-0.6,0.6
+                    <polygon className="dekorator__svg__hvit" points="0.7,67.9 10.2,44.6 19.2,44.6 9.8,67.9  " />
+                    <polygon className="dekorator__svg__hvit" points="117,67.9 126.3,44.6 131.2,44.6 121.9,67.9  " />
+                    <polygon className="dekorator__svg__hvit" points="135.3,67.9 144.6,44.6 147.2,44.6 137.9,67.9    " />
+                    <path className="dekorator__svg__svart" d="M108.4,44.6h-8.2c0,0-0.6,0-0.8,0.5L94.9,59l-4.5-13.9c-0.2-0.5-0.8-0.5-0.8-0.5H73.9c-0.3,0-0.6,0.3-0.6,0.6
         V50c0-3.7-4-5.3-6.3-5.3c-5.2,0-8.7,3.4-9.8,8.7c-0.1-3.5-0.3-4.7-1.3-6c-0.4-0.6-1-1.1-1.7-1.6c-1.4-0.8-2.6-1.1-5.3-1.1h-3.1
         c0,0-0.6,0-0.8,0.5l0,0L42,52.2v-7c0-0.3-0.3-0.6-0.6-0.6h-7.3c0,0-0.6,0-0.8,0.5l-3,7.4c0,0-0.3,0.7,0.4,0.7h2.8v14.1
         c0,0.3,0.3,0.6,0.6,0.6h7.2c0.3,0,0.6-0.3,0.6-0.6V53.2h2.8c1.6,0,2,0,2.6,0.3c0.4,0.1,0.7,0.4,0.9,0.8c0.4,0.7,0.5,1.6,0.5,4.1
