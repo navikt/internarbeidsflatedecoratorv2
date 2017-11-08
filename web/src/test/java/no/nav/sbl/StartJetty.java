8b27e237c48e34a6c301c6762970cb7558dc503b
Johan Rusvik <johan.rusvik@nav.no>
2017-11-08 12:17:39 +0100

    Fjerner ubrukt variabel

--- web/src/test/java/no/nav/sbl/StartJetty.java
+++ web/src/test/java/no/nav/sbl/StartJetty.java
@@ -7,7 +7,6 @@ import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 
 public class StartJetty {
     private static final int PORT = 8186;
-    private static final int ssl_PORT = 8187;
 
     public static void main(String[] args) throws Exception {
         Jetty jetty = usingWar()
