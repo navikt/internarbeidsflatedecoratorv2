8d97f3bb6b3363e6c9e81beca419b5a4c35db42b
Andreas Lund <andreas.lund@nav.no>
2018-02-02 13:12:19 +0100

    fiks så dette bygger ok

--- web/src/frontend/test/components/MenyTest.js
+++ web/src/frontend/test/components/MenyTest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount, shallow } from 'enzyme';
-import Meny, { FunksjonsomradeLenker, AndreSystemerLenker } from '../../js/components/Meny';
+import Meny, { AndreSystemerLenker } from '../../js/components/Meny';
 
 describe("Meny", () => {
     it("Renderer null om lukket", () => {
@@ -15,31 +15,6 @@ describe("Meny", () => {
         expect(meny.find('.dekorator__kolonner')).to.have.length(1);
         expect(meny.find('.dekorator__rad')).to.have.length(1);
         expect(meny.find('section')).to.have.length(3);
-    });
-
-    describe('FunksjonsomradeLenker', () => {
-        const kolonneLenge = [8, 8];
-        it('skal ha to kolonner', () => {
-            const meny = shallow(<FunksjonsomradeLenker fnr="01234567890" enhet={{enhetId: ''}} />);
-
-            const kolonner = meny.find('section.dekorator__kolonne');
-
-            expect(meny.find('.dekorator__kolonner')).to.have.length(1);
-            expect(kolonner).to.have.length(2);
-
-            kolonner.forEach((kolonne, index) => {
-                const header = kolonne.find('.dekorator__lenkeheader');
-                const liste = kolonne.find('.dekorator__menyliste');
-                const lenker = kolonne.find('.dekorator__menylenke');
-
-                expect(header).to.have.length(1);
-                expect(liste).to.have.length(1);
-                expect(lenker).to.have.length(kolonneLenge[index]);
-
-                expect(header.type()).to.equal('h2');
-                expect(liste.type()).to.equal('ul');
-            });
-        });
     });
 
     describe('AndreSystemerLenker', () => {
