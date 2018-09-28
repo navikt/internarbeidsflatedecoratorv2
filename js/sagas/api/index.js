6801ee8c43eb017eeb3d5db73aea8b75ce1aedb3
Ketil Velle <ketil.velle@bekk.no>
2018-09-28 12:13:19 +0200

    OPP-413
    Endret header til nytt navn og genererer random CallId

--- js/sagas/api/index.js
+++ js/sagas/api/index.js
@@ -3,8 +3,8 @@ export function getWithHeaders(url, fnr) {
         credentials: 'include',
         headers: {
             'Nav-Consumer-Id': 'internarbeidsflatedecorator',
-            'Nav-Call-Id': 'todo',
-            'Nav-Identer': fnr,
+            'Nav-Call-Id': randomCallId(),
+            'Nav-Personidenter': fnr,
         },
     })
         .then((res) => {
@@ -62,4 +62,10 @@ export function post(url, body) {
         .catch((err) => {
             throw err;
         });
+}
+
+function randomCallId() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
