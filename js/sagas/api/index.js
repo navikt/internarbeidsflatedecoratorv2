06d734af68f9d06e303c528458d60cf793e889ae
Ketil Velle <ketil.velle@bekk.no>
2018-09-28 12:24:07 +0200

    OPP-413 lint

--- js/sagas/api/index.js
+++ js/sagas/api/index.js
@@ -1,3 +1,9 @@
+function randomCallId() {
+    return ([1e7] + - 1e3 + - 4e3 + - 8e3 + - 1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 export function getWithHeaders(url, fnr) {
     return fetch(url, {
         credentials: 'include',
@@ -62,10 +68,4 @@ export function post(url, body) {
         .catch((err) => {
             throw err;
         });
-}
-
-function randomCallId() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
 }
