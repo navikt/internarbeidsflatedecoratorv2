67fd56e0f8e4e15f658e2dd2da5bdbb9971dc732
Ketil Velle <ketil.velle@bekk.no>
2018-12-06 08:55:53 +0100

    OPP-623: Lagt til lenker til stillingsøk og rekrutteringsbistand.

--- js/sagas/util.js
+++ js/sagas/util.js
@@ -1,3 +1,6 @@
+export const NAIS_PREPROD_SUFFIX = 'preprod.local/';
+export const NAIS_PROD_SUFFIX = 'adeo.no/';
+
 export const finnMiljoStreng = () => {
     const host = window.location.host;
     const bindestrekIndex = host.indexOf('-');
@@ -6,4 +9,23 @@ export const finnMiljoStreng = () => {
     }
     const dotIndex = host.indexOf('.');
     return host.substring(bindestrekIndex, dotIndex);
+};
+
+export const finnNaisMiljoStreng = () => {
+    const host = window.location.host;
+    const bindestrekIndex = host.indexOf('-');
+    if (bindestrekIndex === -1) {
+        return NAIS_PROD_SUFFIX;
+    }
+    return NAIS_PREPROD_SUFFIX;
+
+};
+
+export const finnStillingMiljoStreng = () => {
+    const host = window.location.host;
+    const bindestrekIndex = host.indexOf('-');
+    if (bindestrekIndex === -1) {
+        return '';
+    }
+    return '-q';
 };
