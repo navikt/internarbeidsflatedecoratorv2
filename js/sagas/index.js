d9577fdb62bd1a3d6fca012a54129efc5a3b905b
Ketil Velle <ketil.s.velle@nav.no>
2019-02-26 07:57:40 +0100

    OPP-413 Lagt til aktorSaga igjen nå som VL har laget landingsside for oss.

--- js/sagas/index.js
+++ js/sagas/index.js
@@ -1,11 +1,11 @@
 import veilederSaga from './veilederSaga';
 import enheterSaga from './enheterSaga';
+import aktorSaga from './aktorSaga';
 
 export default function * rootSaga() {
     yield [
         veilederSaga(),
         enheterSaga(),
-        //Frem til VL har laget landingsside: https://jira.adeo.no/browse/PFP-1762
-        //aktorSaga(),
+        aktorSaga(),
     ];
 }
