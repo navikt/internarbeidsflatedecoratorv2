ef5ded485f465da51907583ea57e5b09b3aaa017
Sneha <desaisneha.2011@gmail.com>
2019-03-01 15:22:04 +0100

    [FO-1909] Endret arbeidssokerregistrering lenke

--- js/menyConfig.js
+++ js/menyConfig.js
@@ -61,9 +61,9 @@ export const funksjonsomradeLenker = (fnr, enhet) => [
                 tittel: 'Finn fastlege',
                 url: `${appDomain}/fastlege/`,
             },
-            !window.location.hostname.startsWith('app-q') ? undefined : {
+            window.location.hostname.indexOf('-q') === -1 ? undefined : {
                 tittel: 'Registrer arbeidssøker',
-                url: `${appDomain}/arbeidssokerregistrering/?${fnr ? `fnr=${fnr}` : ''}${fnr && enhet ? '&' : ''}${enhet ? `enhetId=${enhet}` : ''}`,
+                url: `https://arbeidssokerregistrering${finnMiljoStreng()}${naisDomain}?${fnr ? `fnr=${fnr}` : ''}${fnr && enhet ? '&' : ''}${enhet ? `enhetId=${enhet}` : ''}`,
             },
         ],
     },
