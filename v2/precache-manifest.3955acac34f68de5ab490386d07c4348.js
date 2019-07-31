d51801a75df50242d33d2873ec35b560c4f66922
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 09:11:51 +0000

    Deploy navikt/internarbeidsflatedecorator to github.com/navikt/internarbeidsflatedecorator.git:gh-pages

---
+++ v2/precache-manifest.3955acac34f68de5ab490386d07c4348.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753c9e341863a7f06dfe22cb43acae78",
+    "url": "/internarbeidsflatedecorator/v2/index.html"
+  },
+  {
+    "revision": "51da155d17a27b328d35",
+    "url": "/internarbeidsflatedecorator/v2/static/css/main.css"
+  },
+  {
+    "revision": "51da155d17a27b328d35",
+    "url": "/internarbeidsflatedecorator/v2/static/js/head.v2.min.js"
+  }
+]);
