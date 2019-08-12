adca0c57e30bdd17b0457e47ff8089eb22a917d9
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-08-12 11:23:57 +0200

    KAIZEN-0 La til støtte for IE via msCrypto

--- v2/src/utils/url-utils.ts
+++ v2/src/utils/url-utils.ts
@@ -20,9 +20,16 @@ export const finnNaisMiljoStreng = () => {
     return NAIS_PREPROD_SUFFIX;
 };
 
-export function randomCallId(): string {
+declare global {
+    interface Window {
+        msCrypto: {}
+    }
+}
+
+export function randomCallId() {
     let idx = 0;
-    const rnd = Array.from(crypto.getRandomValues(new Uint8Array(32))).map((value) =>
+    const c = window.crypto || window.msCrypto;
+    const rnd = Array.from(c.getRandomValues(new Uint8Array(32))).map((value) =>
         (value & 15).toString(16)
     );
     return '00000000-0000-0000-0000-000000000000'.replace(/0/g, () => rnd[idx++]);
