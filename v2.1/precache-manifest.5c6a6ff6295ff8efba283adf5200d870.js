13111a4be81f7d095aa47498602df98429fcf36a
nutgaard <nutgaard@users.noreply.github.com>
2020-03-02 11:13:32 +0000

    Automated deployment: Mon Mar  2 11:13:32 UTC 2020 0c84761750cba6764053dc4ca850b4bcc8c402a2

---
+++ v2.1/precache-manifest.5c6a6ff6295ff8efba283adf5200d870.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d35d7797fa1e71c54eee90b193689b1",
+    "url": "/internarbeidsflatedecorator/v2.1/index.html"
+  },
+  {
+    "revision": "9bd4cf5bc5cb2f31ea48",
+    "url": "/internarbeidsflatedecorator/v2.1/static/css/main.css"
+  },
+  {
+    "revision": "9bd4cf5bc5cb2f31ea48",
+    "url": "/internarbeidsflatedecorator/v2.1/static/js/head.v2.min.js"
+  }
+]);
