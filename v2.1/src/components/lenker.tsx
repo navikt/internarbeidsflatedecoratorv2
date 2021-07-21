e4422b4e171756a177ff9093ed81475f1855aa54
Nicklas Utgaard <nutgaard@gmail.com>
2021-07-21 13:35:37 +0200

    [KAIZEN-0] bruker app.adeo.no domene for veilarblogin
    må gjøres slik at redirect_url fra isso blir riktig

--- v2.1/src/components/lenker.tsx
+++ v2.1/src/components/lenker.tsx
@@ -45,7 +45,7 @@ const pesysUrl = (fnr: string, path: string) => (fnr ? pesysDomain(path) : pesys
 const gosysUrl = (fnr: string, path: string) => fnr ? gosysDomain(path) : gosysDomain('/gosys/');
 const foreldrePengerUrl = (aktoerId: string, path: string) => aktoerId ? appDomain(path) : appDomain('/fpsak/');
 const byggArbeidssokerregistreringsURL = (fnr: string, enhet: string) => `https://arbeidssokerregistrering${finnMiljoStreng()}${naisDomain}?${fnr ? `fnr=${fnr}` : ''}${fnr && enhet ? '&' : ''}${enhet ? `enhetId=${enhet}` : ''}`;
-const arbeidssokerregistreringLoginUrl = (fnr: string, enhet: string) => `https://veilarblogin${finnMiljoStreng()}${naisDomain}/veilarblogin/api/start?url=${encodeURIComponent(byggArbeidssokerregistreringsURL(fnr, enhet))}`
+const arbeidssokerregistreringLoginUrl = (fnr: string, enhet: string) => appDomain(`/veilarblogin/api/start?url=${encodeURIComponent(byggArbeidssokerregistreringsURL(fnr, enhet))}`)
 const arbeidstreningDomain = `https://arbeidsgiver${finnNaisMiljoStreng()}`;
 const inst2 = () => `https://inst2-web${finnNaisMiljoStreng(true)}/`;
 function k9Url(aktorId: string): string {
