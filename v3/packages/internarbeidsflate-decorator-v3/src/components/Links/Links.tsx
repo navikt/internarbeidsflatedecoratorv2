d04346606ad73910e4a7399b212fa0c2dab7bef3
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2024-01-30 09:06:44 +0100

    mer padding på linker

--- v3/packages/internarbeidsflate-decorator-v3/src/components/Links/Links.tsx
+++ v3/packages/internarbeidsflate-decorator-v3/src/components/Links/Links.tsx
@@ -35,7 +35,7 @@ export default Links;
 const Column: React.FC<{ linkSection: LinkSection }> = ({ linkSection }) => {
   return (
     <section className="dr-min-w-[25%] dr-px-6">
-      <h2 className="dr-font-bold dr-border-white dr-border-solid dr-border-b dr-mb-2">
+      <h2 className="dr-font-bold dr-border-white dr-border-solid dr-border-b dr-mb-2 dr-pb-1">
         {linkSection.title}
       </h2>
       <ul>
