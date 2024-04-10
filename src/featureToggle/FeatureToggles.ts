be9c354f28d59ab1f5440c352d365ed79d5b0baf
Ludvig Hozman <ludvig123@gmail.com>
2024-04-10 10:26:42 +0200

    Delete old version files

--- src/featureToggle/FeatureToggles.ts
+++
@@ -1,3 +0,0 @@
-export enum FeatureToggles {
-    NY_ARBEIDSSOKER_REGISTRERING_URL = 'modiacontextholder.ny-arbeidssoker-registrering-url'
-}
--- src/utils/test.utils.ts
+++
@@ -1,7 +0,0 @@
-export function withLocation(host: string, assertion: () => void) {
-    const original = window.location;
-    delete window.location;
-    window.location = { host } as any;
-    assertion();
-    window.location = original;
-}
