b9cc77d35ee7d0389067a39ec8f8612f55c116ca
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-05-03 13:31:49 +0200

    Dokumenter Vite plugin

--- packages/internarbeidsflate-decorator-v3/vite.config.ts
+++ packages/internarbeidsflate-decorator-v3/vite.config.ts
@@ -4,38 +4,36 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 
-export default defineConfig(() => {
-    const htmlPath = process.env.INTERNARBEIDSFLATE_DECORATOR_HTML_PATH || 'index.html';
-
-    return {
-      plugins: [
-        react(),
-        {
-          name: 'html-transform',
-          transformIndexHtml: {
-            order: 'pre',
-            handler() {
-              return readFileSync(resolve(__dirname, htmlPath), 'utf-8');
-            }
-          }
-        }
-      ],
-      build: {
-        cssMinify: true,
-        minify: true,
-        manifest: 'asset-manifest.json',
-        rollupOptions: {
-          input: 'src/index.ts',
-          preserveEntrySignatures: 'exports-only',
-          // external: ["react", "react-dom"],
-          output: {
-            entryFileNames: 'bundle.js',
-            // format: "iife",
-            inlineDynamicImports: true,
-            assetFileNames: '[name].[ext]'
+export default defineConfig({
+    plugins: [
+      react(),
+      // Plugin som velger hvilken html-fil som skal brukes av dev-serveren
+      {
+        name: 'html-transform',
+        transformIndexHtml: {
+          order: 'pre',
+          handler() {
+            const htmlPath = process.env.INTERNARBEIDSFLATE_DECORATOR_HTML_PATH || 'index.html';
+            return readFileSync(resolve(__dirname, htmlPath), 'utf-8');
           }
         }
       }
-    };
+    ],
+    build: {
+      cssMinify: true,
+      minify: true,
+      manifest: 'asset-manifest.json',
+      rollupOptions: {
+        input: 'src/index.ts',
+        preserveEntrySignatures: 'exports-only',
+        // external: ["react", "react-dom"],
+        output: {
+          entryFileNames: 'bundle.js',
+          // format: "iife",
+          inlineDynamicImports: true,
+          assetFileNames: '[name].[ext]'
+        }
+      }
+    }
   }
 );
