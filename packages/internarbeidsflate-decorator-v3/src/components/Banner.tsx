cce6acb7d7e3ddcd61c357b0a43b7d3069cdb00c
Ludvig Hozman <ludvig123@gmail.com>
2024-09-30 13:08:04 +0200

    Små tweaks på designendringer
    - Fjern klikkbar header
    - Bruk Aksel Search komponenter
    - Fjern custom styling og overskriv tokens istedenfor.

--- packages/internarbeidsflate-decorator-v3/src/components/Banner.tsx
+++ packages/internarbeidsflate-decorator-v3/src/components/Banner.tsx
@@ -13,7 +13,7 @@ const Banner: React.FC = () => {
   return (
     <div className="dr-bg-background dr-text-white dr-flex dr-justify-center">
       <InternalHeader className="dr-max-w-screen-hd dr-w-full dr-items-center">
-        <InternalHeader.Title className="dr-h-full">
+        <InternalHeader.Title as="h1" className="dr-h-full">
           {appName}
         </InternalHeader.Title>
         <div className="dr-flex dr-flex-1 dr-gap-4 xl:dr-gap-8 dr-w-full dr-justify-center dr-items-center">
